975ff2b76781a597028f9157af2d4a7f10ae6376
yoogail105 <yoogail105@gmail.com>
2022-06-05 02:54:06 +0900

    [Feat] New Project Photo

---
+++ Photo/Photo/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photo
+//
+//  Created by 성민주민주 on 2022/06/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Photo/Photo/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  Photo
+//
+//  Created by 성민주민주 on 2022/06/05.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.backgroundColor = .systemBackground
+        let mainViewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: mainViewController)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+}
+
